a32d2b34926d34d5cdd0d17595e07bbdfa5d31c1
mmateoabreu70 <mmateoabreu70@gmail.com>
2020-03-12 08:55:09 -0400

    Terminando la parte del permiso

--- app/src/main/java/com/example/itlavision/MainActivity.java
+++ app/src/main/java/com/example/itlavision/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.itlavision;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
     Button qrBtn;
 
     @Override
@@ -18,15 +24,34 @@ public class MainActivity extends AppCompatActivity {
 
         qrBtn = findViewById(R.id.QrBtn);
 
-        qrBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    }
 
-                Intent qr = new Intent(getApplicationContext(), ScannerQR.class);
-                startActivity(qr);
+
+    public void Escanear(View view) {
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED){
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)){
+
+                Toast.makeText(MainActivity.this, "No puedes escanear si no das permiso", Toast.LENGTH_SHORT).show();
             }
-        });
+            else {
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+
+            }
+        } else {
+
+            Intent qr = new Intent(getApplicationContext(), ScannerQR.class);
+            startActivity(qr);
+        }
 
     }
+
+
+
 
 }
--- app/src/main/java/com/example/itlavision/ScannerQR.java
+++ app/src/main/java/com/example/itlavision/ScannerQR.java
@@ -10,5 +10,8 @@ public class ScannerQR extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner_qr);
+
+
+
     }
 }
