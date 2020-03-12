991d5abb66c227116aa3bfbb5151613d2bed42da
mmateoabreu70 <mmateoabreu70@gmail.com>
2020-03-12 13:28:33 -0400

    Version beta del escaner

--- app/src/main/java/com/example/itlavision/ScannerQR.java
+++ app/src/main/java/com/example/itlavision/ScannerQR.java
@@ -2,16 +2,62 @@ package com.example.itlavision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import com.google.zxing.Result;
 
-public class ScannerQR extends AppCompatActivity {
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScannerQR extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView escanerZXing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scanner_qr);
+        escanerZXing = new ZXingScannerView(this);
+        //hacer que el contenido de la actividad sea el escaner
+        setContentView(escanerZXing);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        //El manejador de los resultados es la clase misma
+        escanerZXing.setResultHandler(this);
+        escanerZXing.startCamera(); //Comenzar la camara al reanudar la app
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        escanerZXing.stopCamera(); //pausar en onPause
+    }
 
 
+
+    @Override
+    public void handleResult(Result result) {
+        setContentView(R.layout.activity_main);
+
+        String texto = result.getText();
+
+        AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+
+        alerta.setTitle("Resultado");
+
+        alerta.setMessage("Resultado: " + texto);
+
+        alerta.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialog, int id){
+
+                }
+        });
+
+        alerta.show();
+
+        escanerZXing.resumeCameraPreview(this);
 
     }
 }
