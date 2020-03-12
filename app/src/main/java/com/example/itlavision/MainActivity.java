b6f25f50bf095d640f1fdac54b452a6e210dff66
mmateoabreu70 <mmateoabreu70@gmail.com>
2020-03-12 00:21:33 -0400

    Boton Leer QR
    Aqui creo el boton para el qr

--- app/src/main/java/com/example/itlavision/MainActivity.java
+++ app/src/main/java/com/example/itlavision/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.itlavision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button qrBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        qrBtn = findViewById(R.id.QrBtn);
+
+        qrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent qr = new Intent(getApplicationContext(), ScannerQR.class);
+                startActivity(qr);
+            }
+        });
 
     }
+
 }
